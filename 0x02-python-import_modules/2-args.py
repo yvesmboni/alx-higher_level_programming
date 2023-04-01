e0b7ff09483aa43733366ea7ed1c5eb04fc38d59
yvesmboni <yvesmboni250@gmail.com>
2023-04-01 02:41:06 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
+
     n = len(sys.argv)
-    if n == 1:
-        print("{:d} {}.".format(0, "arguments"))
-        elif n == 2:
-            print("{:d} {}:".format(1, "argument"))
-            for i in range(1, n):
-                print("{:d}: {}".format(n - 1, sys.argv[i]))
-                else:
-                    print("{:d} {}:".format(n - 1, "arguments"))
-                    for i in range(1, n):
-                        print("{:d}: {}".format(i, sys.argv[i]))
+    s = "s" if n == 1 or n > 2 else ""
+    p = ":" if n > 1 else "."
+
+    p = ":" if n > 1 else "."
+
+    for i in range(1, n):
+        print("{:d}: {}".format(i, sys.argv[i]))
