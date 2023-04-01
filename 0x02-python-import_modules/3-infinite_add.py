a68a6f9ee65de652e5313c0bbe7ba6bdea256ed9
yvesmboni <yvesmboni250@gmail.com>
2023-04-01 02:23:48 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-            print("{:d}".format(add))
+import sys
 
-            if __name__ == "__main__":
-                import sys
-                add_arg(sys.argv)
-    
+if __name__ == "__main__":
+
+    total = 0
+
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+
+        print(total)
