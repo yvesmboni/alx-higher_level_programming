0d65e3cdc583322bebbb37869e8bdcb579c3bb14
yvesmboni <yvesmboni250@gmail.com>
2024-03-21 20:06:14 -0700

    excuted tests

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
-    if key not in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        for keys in a_dictionary:
-            if keys == key:
-                a_dictionary[keys] = value
-                return a_dictionary
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
+
 def multiply_by_2(a_dictionary):
-    multiple = {}
-    for i in a_dictionary:
-        multiple[i] = a_dictionary[i] * 2
-        return multiple
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
